cb25d651578dc91e659a2a081a6e239568ce929d
Anais Augu <augu.anais@gmail.com>
2023-01-19 08:20:49 +0100

    base

---
+++ libft/ft_lstadd_back.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaugu <aaugu@student.42lausanne.ch>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/07 15:47:32 by aaugu             #+#    #+#             */
+/*   Updated: 2022/12/17 17:53:33 by aaugu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+Adds the node ’new’ at the end of the list.
+
+lst: The address of a pointer to the first link of a list.
+new: The address of a pointer to the node to be added to the list.
+*/
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	ft_lstlast(*lst)->next = new;
+}

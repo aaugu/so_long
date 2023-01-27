19a13cba0677a39335043e87a61e2161c5ca2504
Anais Augu <augu.anais@gmail.com>
2023-01-27 10:40:55 +0100

    is_map_valid change

--- src/check_map.c
+++ src/check_map.c
@@ -6,7 +6,7 @@
 /*   By: aaugu <aaugu@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 11:49:14 by aaugu             #+#    #+#             */
-/*   Updated: 2023/01/26 16:25:18 by aaugu            ###   ########.fr       */
+/*   Updated: 2023/01/27 10:40:02 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,11 @@
 
 t_bool	is_map_valid(const char **map)
 {
-	int	bool;
-
-	bool = 1;
 	if (!is_rectangular(*map) || !is_closed(*map) || !has_all_elements(*map) \
 	|| !is_solvable(*map))
 	{
 		ft_printf("Error, map is not valid\n");
 		return (0);
 	}
-	return (bool);
+	return (1);
 }

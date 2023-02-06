2b478bb680929f3a84af94d5983aaf2979a5abee
Anais Augu <aaugu@c2r9s5.42lausanne.ch>
2023-02-06 14:35:51 +0100

    nb of player and exit not initialised at 0 fixed

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: aaugu <aaugu@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 10:13:06 by aaugu             #+#    #+#             */
-/*   Updated: 2023/02/06 14:04:48 by aaugu            ###   ########.fr       */
+/*   Updated: 2023/02/06 14:33:14 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,9 +49,9 @@ typedef struct s_tileset
 typedef struct s_data
 {
 	int		moves;
-	int		nb_apples;
-	int		nb_exit;
-	int		nb_player;
+	int		apple;
+	int		exit;
+	int		player;
 }				t_data;
 
 typedef struct t_game
@@ -80,13 +80,14 @@ t_bool	is_extension_valid(const char *filename);
 char	**map_parsing(const char *filename);
 int		count_lines(const char *filename);
 char	**ft_freeall(char **strs);
+void	map_data_init(t_game *game);
 
 /* ---------------	MAP REQUIREMENTS CHECK	--------------- */
 t_bool	is_map_valid(char **map, t_game *game);
 t_bool	is_rect(char **map, t_game *game);
 t_bool	is_closed(char **map);
 t_bool	is_closed_horizontal(char *map);
-t_bool	is_complete(char **map, t_game *game);
+t_bool	is_complete(char **map, t_game *game, int x, int y);
 
 /* ---------------	MAP SOLVABLE CHECK	--------------- */
 t_bool	is_solvable(char **map);
--- src/check_file.c
+++ src/check_file.c
@@ -6,7 +6,7 @@
 /*   By: aaugu <aaugu@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 11:18:35 by aaugu             #+#    #+#             */
-/*   Updated: 2023/02/06 13:36:02 by aaugu            ###   ########.fr       */
+/*   Updated: 2023/02/06 14:32:38 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ t_bool	is_all_valid(const char *filename, t_game *game)
 	if (!is_extension_valid(filename))
 		return (0);
 	map = map_parsing(filename);
+	map_data_init(game);
 	if (!map || !is_map_valid(map, game))
 		return (0);
 	game->map.layout = map;
@@ -39,4 +40,11 @@ t_bool	is_extension_valid(const char *filename)
 	if (ft_strcmp(extension, ".ber") == 0)
 		return (1);
 	return (0);
+}
+
+void	map_data_init(t_game *game)
+{
+	game->data.apple = 0;
+	game->data.exit = 0;
+	game->data.player = 0;
 }
